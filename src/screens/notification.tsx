2bccc9967913a12aef5812d8956c2d537866ffaa
Jesse Nwaokolo <jungkookyung61@gmail.com>
2026-02-27 15:59:37 +0100

    Fixed navigation reset from notifications to app

--- src/screens/notification.tsx
+++ src/screens/notification.tsx
@@ -4,82 +4,81 @@ import SafeArea from "@/components/SafeArea";
 import { useRouter } from "@/hooks/useRouter";
 import { FONTS } from "@/themes/theme";
 import React from "react";
-import {
-  Text,
-  TouchableOpacity,
-  View
-} from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 const Notification = () => {
   const navigation = useRouter();
   return (
-   <SafeArea>
-
+    <SafeArea>
+      <View style={{ flex: 1 }}>
         <View style={{ flex: 1 }}>
-          <View style={{ flex: 1 }}>
-            {/* icon */}
-            <View
+          {/* icon */}
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginBottom: 40,
+            }}
+          >
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <BackIcon />
+            </TouchableOpacity>
+            <QuestionMarkIcon />
+          </View>
+
+          {/* title */}
+          <View>
+            <Text
               style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                marginBottom: 40,
+                fontFamily: FONTS.semiBold,
+                fontSize: 20,
+                color: "#0D0D0D",
+                marginBottom: 8,
               }}
             >
-              <TouchableOpacity onPress={() => navigation.goBack()}>
-                <BackIcon />
-              </TouchableOpacity>
-              <QuestionMarkIcon />
-            </View>
-
-            {/* title */}
-            <View>
-              <Text
-                style={{
-                  fontFamily: FONTS.semiBold,
-                  fontSize: 20,
-                  color: "#0D0D0D",
-                  marginBottom: 8,
-                }}
-              >
-                Lastly, please enable notification
-              </Text>
-              <Text
-                style={{
-                  fontFamily: FONTS.regular,
-                  fontSize: 16,
-                  color: "#777777",
-                  marginBottom: 20,
-                }}
-              >
-                Enable your notifications for more update and important messages
-                about your grocery needs
-              </Text>
-            </View>
-
-            {/* contents */}
-            <View
+              Lastly, please enable notification
+            </Text>
+            <Text
               style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
+                fontFamily: FONTS.regular,
+                fontSize: 16,
+                color: "#777777",
+                marginBottom: 20,
               }}
             >
-              <PhoneIcon />
-            </View>
+              Enable your notifications for more update and important messages
+              about your grocery needs
+            </Text>
+          </View>
+
+          {/* contents */}
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <PhoneIcon />
           </View>
         </View>
+      </View>
 
-        {/* button */}
-        <View style={{ gap: 16, marginBottom: 48 }}>
-          <Button
-            title="Enable Notifications"
-            onPress={() => navigation.replace("app", { screen: "home" })}
-          />
-          <Button title="Skip For Now" variant="secondary" />
-        </View>
-        </SafeArea>
+      {/* button */}
+      <View style={{ gap: 16, marginBottom: 48 }}>
+        <Button
+          title="Enable Notifications"
+          onPress={() =>
+            navigation.reset({
+              index: 0,
+              routes: [{ name: "app" }],
+            })
+          }
+        />
+        <Button title="Skip For Now" variant="secondary" />
+      </View>
+    </SafeArea>
   );
 };
 
 export default Notification;
-
